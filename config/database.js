f520de9a549e2e9c4bb73649c114a969db8cc61f
ZK3DEV <z.k@urban.co>
2020-04-20 23:49:50 +0300

    OB-3 change local db info

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 // local test db
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('open-eshop', 'root', '', {
+const sequelize = new Sequelize('open-eshop', 'root', 'open123', {
   host: 'localhost',
-  port: '32780', // change port to 3306 in future
+  port: '32788', // change port to 3306 in future
   dialect: 'mysql',
   operatorsAliases: false,
   pool: {
